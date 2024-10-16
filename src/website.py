b5c3a78856174620ab7e67211e84cb48520719cb
SajjiKazemi <SajjadKazemi.e@gmail.com>
2024-10-16 18:07:24 -0400

    Complement Website class by adding new attributes and methods.

--- src/website.py
+++ src/website.py
@@ -5,8 +5,17 @@ class Website:
     def __init__(self, url: str):
         self.url = url
         response = requests.get(url=url)
-        soup = BeautifulSoup(response.content, 'html.parser')
+        self.body = response.content
+        soup = BeautifulSoup(self.body, 'html.parser')
         self.title = soup.title.string if soup.title else 'No title found'
-        for irrelevant in soup.body(["script", "style", "img", "input"]):
-            irrelevant.decompose()
-        self.text = soup.body.get_text(separator="\n", strip=True)
+        if soup.body:
+            for irrelevant in soup.body(["script", "style", "img", "input"]):
+                irrelevant.decompose()
+            self.text = soup.body.get_text(separator="\n", strip=True)
+        else:
+            self.text = ""
+        links = [link.get('href') for link in soup.find_all('a')]
+        self.links = [link for link in links if link]
+    
+    def get_contents(self):
+        return f"Webpage Title:\n{self.title}\nWebpaga Contents:\n{self.text}\n\n"
