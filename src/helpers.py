f9bc4821d99c3b9ba29f8988f0881ba4cf656d00
SajjiKazemi <SajjadKazemi.e@gmail.com>
2024-10-16 18:17:42 -0400

    Add a function to create the user prompt by keeping the website's links.

--- src/helpers.py
+++ src/helpers.py
@@ -22,3 +22,11 @@ def summarize(url: str, openai: OpenAI, system_prompt: str):
         messages = messages_for(website, system_prompt) 
     )
     return response.choices[0].message.content
+
+def get_links_user_prompt(website: Website):
+        user_prompt = f"Here is the list of links on the website of {website.url} - "
+        user_prompt += "please decide which of these are relevant web links for a brochure about the company, respond with the full https URL in JSON format. \
+    Do not include Terms of Service, Privacy, email links.\n"
+        user_prompt += "Links (some might be relative links):\n"
+        user_prompt += "\n".join(website.links)
+        return user_prompt
