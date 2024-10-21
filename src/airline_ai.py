a6b49d5d4af18c469ab5aff4fcd0c83fc69b67e6
SajjiKazemi <SajjadKazemi.e@gmail.com>
2024-10-21 19:30:39 -0400

    Add image and voice generation.

--- src/airline_ai.py
+++ src/airline_ai.py
@@ -3,6 +3,11 @@ import json
 from dotenv import load_dotenv
 from openai import OpenAI
 import gradio as gr
+import base64
+from io import BytesIO
+from PIL import Image
+from pydub import AudioSegment
+from pydub.playback import play
 
 load_dotenv()
 os.environ['OPENAI_API_KEY'] = os.getenv('OPENAI_API_KEY', 'your-key-if-not-using-env')
@@ -50,7 +55,30 @@ def handle_tool_call(message):
     }
     return response, city
 
+def artist(city):
+    image_response = openai.images.generate(
+        model="dall-e-3",
+        prompt=f"An image representing a vacation in {city}, showing tourist spots and everything unique about {city}, in a vibrant pop-art style",
+        size="1024x1024",
+        n=1,
+        response_format="b64_json",
+    )
+    image_base64 = image_response.data[0].b64_json
+    image_data = base64.b64decode(image_base64)
+    return Image.open(BytesIO(image_data))
+
+def talker(message):
+    response = openai.audio.speech.create(
+        model="tts-1",
+        voice="onyx",
+        input=message
+    )
+    audio_stream = BytesIO(response.content)
+    audio = AudioSegment.from_file(audio_stream, format="mp3")
+    play(audio)
+
 def chat(message, history):
+    image = None
     messages = [{"role": "system", "content": system_message}]
     for human, assistant in history:
         messages.append({"role": "user", "content": human})
@@ -63,8 +91,35 @@ def chat(message, history):
         response, city = handle_tool_call(message)
         messages.append(message)
         messages.append(response)
+        image = artist(city)
         response = openai.chat.completions.create(model=MODEL, messages=messages)
-    
-    return response.choices[0].message.content
 
-gr.ChatInterface(fn=chat).launch()
+    reply = response.choices[0].message.content
+    talker(reply)
+    return reply, image
+
+
+with gr.Blocks() as ui:
+    with gr.Row():
+        chatbot = gr.Chatbot(height=500)
+        image_output = gr.Image(height=500)
+    with gr.Row():
+        msg = gr.Textbox(label="Chat with out AI Assistant")
+    with gr.Row():
+        clear = gr.Button("Clear")
+    
+    def user(user_message, history):
+        return "", history + [[user_message, None]]
+
+    def bot(history):
+        user_message = history[-1][0]
+        bot_message, image = chat(user_message, history[:-1])
+        history[-1][1] = bot_message
+        return history, image
+
+    msg.submit(user, [msg, chatbot], [msg, chatbot], queue=False).then(
+        bot, chatbot, [chatbot, image_output]
+    )
+    clear.click(lambda: None, None, chatbot, queue=False)
+
+ui.launch(share=True)
