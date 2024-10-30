85f6eb4e56b5ef35f54cfa634e9904a4aaa88174
SajjiKazemi <SajjadKazemi.e@gmail.com>
2024-10-30 17:54:21 -0400

    Add the code to get different categories.

--- src/curate_data.py
+++ src/curate_data.py
@@ -20,4 +20,63 @@ login(hf_token, add_to_git_credential=True)
 
 items = ItemLoader("Appliances").load()
 
-print(items[1].prompt)
+#print(items[1].prompt)
+
+dataset_names = [
+    "Automotive",
+    "Electronics",
+    "Office_Products",
+    "Tools_and_Home_Improvement",
+    "Cell_Phones_and_Accessories",
+    "Toys_and_Games",
+    "Appliances",
+    "Musical_Instruments",
+]
+
+items = []
+for dataset_name in dataset_names:
+    loader = ItemLoader(dataset_name)
+    items.extend(loader.load())
+
+print(f"A grand total of {len(items):,} items")
+
+# Plot the distribution of token counts again
+tokens = [item.token_count for item in items]
+plt.figure(figsize=(15, 6))
+plt.title(f"Token counts: Avg {sum(tokens)/len(tokens):,.1f} and highest {max(tokens):,}\n")
+plt.xlabel('Length (tokens)')
+plt.ylabel('Count')
+plt.hist(tokens, rwidth=0.7, color="skyblue", bins=range(0, 300, 10))
+plt.show()
+
+# Plot the distribution of prices
+prices = [item.price for item in items]
+plt.figure(figsize=(15, 6))
+plt.title(f"Prices: Avg {sum(prices)/len(prices):,.1f} and highest {max(prices):,}\n")
+plt.xlabel('Price ($)')
+plt.ylabel('Count')
+plt.hist(prices, rwidth=0.7, color="blueviolet", bins=range(0, 1000, 10))
+plt.show()
+
+category_counts = Counter()
+for item in items:
+    category_counts[item.category]+=1
+
+categories = category_counts.keys()
+counts = [category_counts[category] for category in categories]
+
+# Bar chart by category
+plt.figure(figsize=(15, 6))
+plt.bar(categories, counts, color="goldenrod")
+plt.title('How many in each category')
+plt.xlabel('Categories')
+plt.ylabel('Count')
+
+plt.xticks(rotation=30, ha='right')
+
+# Add value labels on top of each bar
+for i, v in enumerate(counts):
+    plt.text(i, v, f"{v:,}", ha='center', va='bottom')
+
+# Display the chart
+plt.show()
