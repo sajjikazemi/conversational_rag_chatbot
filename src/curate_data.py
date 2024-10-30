705c37225a8356cc6d50d7d46374c9c8fca8b2d5
SajjiKazemi <SajjadKazemi.e@gmail.com>
2024-10-30 18:46:05 -0400

    Add code to create training and test dataset and pickle it.

--- src/curate_data.py
+++ src/curate_data.py
@@ -97,44 +97,79 @@ for i in range(1, 1000):
         sample.extend(slot)
     else:
         weights = np.array([1 if item.category=='Automotive' else 5 for item in slot])
-        weights = weights / np.sun(weights)
+        weights = weights / np.sum(weights)
         selected_indices = np.random.choice(len(slot), size=1200, replace=False, p=weights)
         selected = [slot[i] for i in selected_indices]
         sample.extend(selected)
 
 print(f"There are {len(sample):,} items in the sample")
 
-# Plot the distribution of prices in sample
-prices = [float(item.price) for item in sample]
-plt.figure(figsize=(15, 10))
-plt.title(f"Avg {sum(prices)/len(prices):.2f} and highest {max(prices):,.2f}\n")
-plt.xlabel('Price ($)')
-plt.ylabel('Count')
-plt.hist(prices, rwidth=0.7, color="darkblue", bins=range(0, 1000, 10))
-plt.show()
+# # Plot the distribution of prices in sample
+# prices = [float(item.price) for item in sample]
+# plt.figure(figsize=(15, 10))
+# plt.title(f"Avg {sum(prices)/len(prices):.2f} and highest {max(prices):,.2f}\n")
+# plt.xlabel('Price ($)')
+# plt.ylabel('Count')
+# plt.hist(prices, rwidth=0.7, color="darkblue", bins=range(0, 1000, 10))
+# plt.show()
 
-# Categories distributions
-category_counts = Counter()
-for item in sample:
-    category_counts[item.category]+=1
+# # Categories distributions
+# category_counts = Counter()
+# for item in sample:
+#     category_counts[item.category]+=1
 
-categories = category_counts.keys()
-counts = [category_counts[category] for category in categories]
+# categories = category_counts.keys()
+# counts = [category_counts[category] for category in categories]
 
-# Create bar chart
-plt.figure(figsize=(15, 6))
-plt.bar(categories, counts, color="lightgreen")
+# # Create bar chart
+# plt.figure(figsize=(15, 6))
+# plt.bar(categories, counts, color="lightgreen")
 
-# Customize the chart
-plt.title('How many in each category')
-plt.xlabel('Categories')
-plt.ylabel('Count')
+# # Customize the chart
+# plt.title('How many in each category')
+# plt.xlabel('Categories')
+# plt.ylabel('Count')
 
-plt.xticks(rotation=30, ha='right')
+# plt.xticks(rotation=30, ha='right')
 
-# Add value labels on top of each bar
-for i, v in enumerate(counts):
-    plt.text(i, v, f"{v:,}", ha='center', va='bottom')
+# # Add value labels on top of each bar
+# for i, v in enumerate(counts):
+#     plt.text(i, v, f"{v:,}", ha='center', va='bottom')
 
-# Display the chart
-plt.show()
+# # Display the chart
+# plt.show()
+
+# Dividing the data into train and test sets
+random.seed(42)
+random.shuffle(sample)
+train = sample[:400_000]
+test = sample[400_000:402_000]
+print(f"Divided into a training set of {len(train):,} items and test set of {len(test):,} items")
+
+# print(train[0].prompt)
+# print(test[0].test_prompt())
+
+# Uploading the dataset
+train_prompts = [item.prompt for item in train]
+train_prices = [item.price for item in train]
+test_prompts = [item.test_prompt() for item in test]
+test_prices = [item.price for item in test]
+
+train_dataset = Dataset.from_dict({"text": train_prompts, "price": train_prices})
+test_dataset = Dataset.from_dict({"text": test_prompts, "price": test_prices})
+dataset = DatasetDict({
+    "train": train_dataset,
+    "test": test_dataset
+})
+
+# Push the dataset to the hub
+HF_USER = "sajjikazemi"
+DATASET_NAME = f"{HF_USER}/pricer-data"
+dataset.push_to_hub(DATASET_NAME, private=True)
+
+# Pickle the dataset such that we don't have to execute this code again
+with open('train.pkl', 'wb') as file:
+    pickle.dump(train, file)
+
+with open('test.pkl', 'wb') as file:
+    pickle.dump(test, file)
