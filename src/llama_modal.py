d81dc965628804c3b3ac18a384a524e19d9f32e9
SajjiKazemi <SajjadKazemi.e@gmail.com>
2024-11-07 15:31:08 -0500

    Add code to create RAG using SentenceTransformer model.

--- src/llama_modal.py
+++ src/llama_modal.py
@@ -3,6 +3,6 @@ from llama import app, generate
 
 with modal.enable_output():
     with app.run():
+        #result = generate.local("Life is a mystery, everyone must stand alone, I hear")
         result = generate.remote("Life is a mystery, everyone must stand alone, I hear")
-
 print(result)
